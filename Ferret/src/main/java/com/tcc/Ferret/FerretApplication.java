2608fb25ebb77b6c1bcfe32159c57317445a468a
Gabriel/ Geek <79331653+GeekHero22@users.noreply.github.com>
2023-05-18 20:17:58 -0300

    Add files via upload

---
+++ Ferret/src/main/java/com/tcc/Ferret/FerretApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.Ferret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerretApplication.class, args);
+	}
+
+}
